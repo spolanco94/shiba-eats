24d4218ece987eef03fec6d3ad1bb2f0614755d0
Steven Polanco <spolanco2694@gmail.com>
2018-01-24 22:59:25 +0000

    Add user profile pages with Bio, email, avatar, and name forms to sign up. Add forgot password and password reset route.

--- v2.13/models/user.js
+++ v2.13/models/user.js
@@ -4,6 +4,13 @@ var passportLocalMongoose       = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
+    avatar: String,
+    firstName: String,
+    lastName: String,
+    bio: String,
+    email: { type: String, unique: true, required: true },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     isAdmin: {type: Boolean, default: false}
 });
 
